dea198b05627f3d8e501fc5aa0c60b0906fe6908
kovan <kovan.blue@yahoo.com>
2021-06-06 22:00:59 +0300

    Update kucko.js

--- kucko.js
+++ kucko.js
@@ -1,9 +1,9 @@
 
 
 //  Messenger Chat plugin Code
-    <div id="fb-root"></div>
+    //<div id="fb-root"></div>
     var div = document.createElement("div");
-    div.setAttribute(id,"root");
+    div.setAttribute("id","root");
     document.body.appendChild(div);
     // <!-- Your Chat plugin code -->
     // <div id="fb-customer-chat" class="fb-customerchat">
@@ -11,6 +11,7 @@
     var div1 = document.createElement("div");
                div1.setAttribute("id","fb-customer-chat");
                div1.setAttribute("class","fb-customerchat");
+     document.body.appendChild(div1);
     // <script>
       var chatbox = document.getElementById('fb-customer-chat');
       chatbox.setAttribute("page_id", "242499065762421");
